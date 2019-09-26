8595de2d7834a6bfcefbc8d129b285dc17a4c9d1
brighthelianthus <54312877+brighthelianthus@users.noreply.github.com>
2019-09-26 18:01:50 +0530

    Add files via upload

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,12 +1,6 @@
 from movie_recommendation_data import dataset
 from math import sqrt
 
-# print "Lisa Rose rating on Lady in the water: {}\n".format(dataset['Lisa Rose']['Lady in the Water'])
-# print "Michael Phillips rating on Lady in the water: {}\n".format(dataset['Michael Phillips']['Lady in the Water'])
-#
-# print '**************Jack Matthews ratings**************'
-# print dataset['Jack Matthews']
-
 def similarity_score(person1,person2):
 
     # Returns ratio Euclidean distance score of person1 and person2
@@ -35,10 +29,11 @@ def pearson_correlation(person1,person2):
 
     # Returns Pearson Correlation coefficient of person1 and person2
     #To get both rated items
-    both_rated = {}
-    for item in dataset[person2]:
-        if item in dataset[person2]:
-            both_rated[item]=1
+    both_rated = []
+    for i in dataset[person2]:
+        for j in dataset[person1]:
+            if i == j:
+                both_rated.append(i)
 
     number_of_ratings = len(both_rated)
 
@@ -47,15 +42,16 @@ def pearson_correlation(person1,person2):
         return 0
 
     #Add up all the preferences of each user
-    person1_preferences_sum = sum([dataset[person1][item] for item in both_rated])
-    person2_preferences_sum = sum([dataset[person2][item] for item in both_rated])
+
+    person1_preferences_sum = sum(dataset[person1][item] for item in both_rated)
+    person2_preferences_sum = sum(dataset[person2][item] for item in both_rated)
 
     # sum of squares of preferences squares of each user
     person1_square_preferences_sum = sum([pow(dataset[person1][item],2)for item in both_rated])
     person2_square_preferences_sum = sum([pow(dataset[person2][item],2)for item in both_rated])
 
     # sum up the product value of both preferences for each item
-    sum_of_product_both_users = sum ([dataset[person1][item]* dataset[person2][item] for item in both_rated])
+    sum_of_product_both_users = sum ([dataset[person1][item] * dataset[person2][item] for item in both_rated])
 
     #calculate the pearson score
 
@@ -63,7 +59,7 @@ def pearson_correlation(person1,person2):
     denominator = sqrt((person1_square_preferences_sum - pow(person1_preferences_sum,2)/number_of_ratings)
                        *(person2_square_preferences_sum - pow(person2_preferences_sum,2)/number_of_ratings))
 
-    if denominator==0:
+    if denominator == 0:
         return 0
     else:
         r = numerator/denominator
@@ -78,19 +74,13 @@ def most_similar_persons(person, number_of_persons):
     # Sort the similar persons so that highest scores person will appear at the first
     scores.sort()
     scores.reverse()
-    return scores[0:]
+    return scores[0: number_of_persons]
 
-#print most_similar_persons('Lisa Rose', 3)      #### using pearson correlation   measure
-
-# print 'Jack Matthews to Lisa Rose using Least Euclidean Distance Ratio :',similarity_score("Lisa Rose","Jack Matthews")      #### using euclidean distance measure
-# print 'Jack Matthews to Lisa Rose using Pearson Correlation :',pearson_correlation("Lisa Rose","Jack Matthews")              #### using pearson correlation   measure
-# print 'Jack Matthews to Gene Seymour using Pearson Correlation :',pearson_correlation('Lisa Rose','Gene Seymour')            #### using pearson correlation   measure
-#print dataset["Lisa Rose"]
 def user_recommendations(person):
     # Gets recommendations for a person by using a weighted average of every other user's ranking
     totals = {}
     simSums = {}
-    print dataset
+    # print dataset
     for other in dataset:
         # don't compare me to myself
         if other == person:
@@ -112,7 +102,8 @@ def user_recommendations(person):
 
                 # sum of similarities
                 simSums.setdefault(item,0)
-                simSums[item]= simSums[item] + sim    # We could just use the totals to calculate
+                simSums[item]= simSums[item] + sim
+                # We could just use the totals to calculate
                 # the rankings, but then a movie reviewed by
                 # more people would have a big advantage.
                 # To correct for this you need to divide by the sum of
@@ -122,10 +113,22 @@ def user_recommendations(person):
     rankings = [(total/simSums[item], item) for item , total in totals.items()]
     rankings.sort()
     rankings.reverse()
-    print rankings
+
     # returns the recommended items
-   # recommendations_list = [recommended_item for score,recommend_item in rankings]
+    recommendations_list = [recommend_item for score,recommend_item in rankings]
+    print "Recommendations for you : "
+    return recommendations_list
 
-user_recommendations('Toby')
+print "All people :"
+for person in dataset :
+    print "\t",person
 
+name =  raw_input("\nEnter your name please : ")
 
+print "Hi,",format(name), "!"
+print user_recommendations(name)
+print "\n People most similar to you : "
+print most_similar_persons(name,3)      #### using pearson correlation   measure
+print '\nJack Matthews to Lisa Rose using Least Euclidean Distance Ratio :',similarity_score("Lisa Rose","Jack Matthews")      #### using euclidean distance measure
+print 'Jack Matthews to Lisa Rose using Pearson Correlation :',pearson_correlation("Lisa Rose","Jack Matthews")              #### using pearson correlation   measure
+print 'Jack Matthews to Gene Seymour using Pearson Correlation :',pearson_correlation('Lisa Rose','Gene Seymour')           #### using pearson correlation   measure
